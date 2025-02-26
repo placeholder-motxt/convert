4045244718034895e13ab21478359ddd520064a0
Clarence Grady <clarence.grady@ui.ac.id>
2025-02-26 22:45:34 +0700

    [RED] Add skeleton for ParseJsonToObject

---
+++ app/parse_json_to_object.py
@@ -0,0 +1,12 @@
+import json
+from app.element_objects import *
+
+class ParseJsonToObject:
+    def __init__(self,data):
+        self.__json = data
+    
+    def parse_methods(self):
+        pass
+
+    def parse_relationships(self):
+        pass
