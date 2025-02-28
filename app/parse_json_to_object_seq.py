ffb118074f5ce0dc942a9772f0ca4db05f1d2049
Will S <williamsusanto82@gmail.com>
2025-02-28 20:40:54 +0700

    [RED] Add skeleton for ParseJsonToObjectSeq

---
+++ app/parse_json_to_object_seq.py
@@ -0,0 +1,14 @@
+import json
+from jsonschema import validate
+from app.element_objects import *
+
+class ParseJsonToObjectSeq:
+    def __init__(self):
+        self.__json = None
+
+    def set_json(self, data):
+        pass
+
+    def validate_json(self, data):
+        pass
+        
