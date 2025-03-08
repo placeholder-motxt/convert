746c4fa9fd0a8230ee63ad3e361af69407fbeb1d
Will S <williamsusanto82@gmail.com>
2025-03-08 22:01:16 +0700

    [RED] Add test for method call object

--- tests/test_parse_json_to_object_seq.py
+++ tests/test_parse_json_to_object_seq.py
@@ -2185,3 +2185,890 @@ class TestParseJsonToObjectSeq(unittest.TestCase):
             parser.set_json(json_data)
             parser.parse()
         self.assertEqual(str(context.exception), "Duplicate attribute!")
+    
+    def test_positive_class_object(self):
+        json_data = """ 
+{
+  "diagram": "SequenceDiagram",
+  "nodes": [
+    {
+      "children": [
+        1
+      ],
+      "name": "buku:Buku",
+      "x": 510,
+      "y": 123,
+      "id": 0,
+      "type": "ImplicitParameterNode"
+    },
+    {
+      "x": 0,
+      "y": 0,
+      "openBottom": false,
+      "id": 1,
+      "type": "CallNode"
+    },
+    {
+      "children": [
+        3
+      ],
+      "name": "listBuku:ListBuku",
+      "x": 370,
+      "y": 91,
+      "id": 2,
+      "type": "ImplicitParameterNode"
+    },
+    {
+      "x": 0,
+      "y": 0,
+      "openBottom": false,
+      "id": 3,
+      "type": "CallNode"
+    },
+    {
+      "children": [
+        5,
+        6,
+        7,
+        8,
+        9,
+        10,
+        11,
+        12,
+        13,
+        14
+      ],
+      "name": ":views",
+      "x": 250,
+      "y": 52,
+      "id": 4,
+      "type": "ImplicitParameterNode"
+    },
+    {
+      "x": 0,
+      "y": 0,
+      "openBottom": false,
+      "id": 5,
+      "type": "CallNode"
+    },
+    {
+      "x": 0,
+      "y": 0,
+      "openBottom": false,
+      "id": 6,
+      "type": "CallNode"
+    },
+    {
+      "x": 0,
+      "y": 0,
+      "openBottom": false,
+      "id": 7,
+      "type": "CallNode"
+    },
+    {
+      "x": 890,
+      "y": 10,
+      "openBottom": false,
+      "id": 8,
+      "type": "CallNode"
+    },
+    {
+      "x": 335,
+      "y": 56,
+      "openBottom": false,
+      "id": 9,
+      "type": "CallNode"
+    },
+    {
+      "x": 29,
+      "y": 29,
+      "openBottom": false,
+      "id": 10,
+      "type": "CallNode"
+    },
+    {
+      "x": 0,
+      "y": 0,
+      "openBottom": false,
+      "id": 11,
+      "type": "CallNode"
+    },
+    {
+      "x": 0,
+      "y": 0,
+      "openBottom": false,
+      "id": 12,
+      "type": "CallNode"
+    },
+    {
+      "x": 0,
+      "y": 0,
+      "openBottom": false,
+      "id": 13,
+      "type": "CallNode"
+    },
+    {
+      "x": 0,
+      "y": 0,
+      "openBottom": false,
+      "id": 14,
+      "type": "CallNode"
+    },
+    {
+      "children": [
+        16
+      ],
+      "name": "copyBuku:CopyBuku",
+      "x": 1420,
+      "y": -8,
+      "id": 15,
+      "type": "ImplicitParameterNode"
+    },
+    {
+      "x": 0,
+      "y": 0,
+      "openBottom": false,
+      "id": 16,
+      "type": "CallNode"
+    },
+    {
+      "children": [
+        18
+      ],
+      "name": "peminjaman:Peminjaman",
+      "x": 840,
+      "y": 28,
+      "id": 17,
+      "type": "ImplicitParameterNode"
+    },
+    {
+      "x": 0,
+      "y": 0,
+      "openBottom": false,
+      "id": 18,
+      "type": "CallNode"
+    },
+    {
+      "children": [
+        20,
+        21
+      ],
+      "name": "listCopy:ListCopy",
+      "x": 1090,
+      "y": 126,
+      "id": 19,
+      "type": "ImplicitParameterNode"
+    },
+    {
+      "x": 0,
+      "y": 0,
+      "openBottom": false,
+      "id": 20,
+      "type": "CallNode"
+    },
+    {
+      "x": 0,
+      "y": 0,
+      "openBottom": false,
+      "id": 21,
+      "type": "CallNode"
+    },
+    {
+      "children": [
+        23
+      ],
+      "name": "peminjam:Peminjam",
+      "x": 800,
+      "y": 73,
+      "id": 22,
+      "type": "ImplicitParameterNode"
+    },
+    {
+      "x": 0,
+      "y": 0,
+      "openBottom": false,
+      "id": 23,
+      "type": "CallNode"
+    },
+    {
+      "children": [
+        25
+      ],
+      "name": ":UI",
+      "x": 10,
+      "y": 65,
+      "id": 24,
+      "type": "ImplicitParameterNode"
+    },
+    {
+      "x": 26,
+      "y": 18,
+      "openBottom": false,
+      "id": 25,
+      "type": "CallNode"
+    },
+    {
+      "children": [
+        27,
+        28
+      ],
+      "name": "listPeminjam:ListPeminjam",
+      "x": 620,
+      "y": 123,
+      "id": 26,
+      "type": "ImplicitParameterNode"
+    },
+    {
+      "x": 0,
+      "y": 0,
+      "openBottom": false,
+      "id": 27,
+      "type": "CallNode"
+    },
+    {
+      "x": 0,
+      "y": 0,
+      "openBottom": false,
+      "id": 28,
+      "type": "CallNode"
+    }
+  ],
+  "edges": [
+    {
+      "middleLabel": "[POST] login (username, password)",
+      "start": 25,
+      "end": 14,
+      "type": "CallEdge",
+      "signal": false
+    },
+    {
+      "middleLabel": "HalamanPemrosesanPeminjaman ()",
+      "start": 25,
+      "end": 5,
+      "type": "CallEdge",
+      "signal": false
+    },
+    {
+      "middleLabel": "LihatDetailBuku (isbn)",
+      "start": 25,
+      "end": 6,
+      "type": "CallEdge",
+      "signal": false
+    },
+    {
+      "middleLabel": "getBuku (isbn)",
+      "start": 6,
+      "end": 3,
+      "type": "CallEdge",
+      "signal": false
+    },
+    {
+      "middleLabel": "FormProsesPeminjaman (isbn)",
+      "start": 25,
+      "end": 7,
+      "type": "CallEdge",
+      "signal": false
+    },
+    {
+      "middleLabel": "[POST] SubmitProsesPeminjaman (isbn)",
+      "start": 25,
+      "end": 8,
+      "type": "CallEdge",
+      "signal": false
+    },
+    {
+      "middleLabel": "isValid (dataAnggota)",
+      "start": 8,
+      "end": 27,
+      "type": "CallEdge",
+      "signal": false
+    },
+    {
+      "middleLabel": "[isValid] prosesPeminjamanValidKeanggotaan ()",
+      "start": 8,
+      "end": 9,
+      "type": "CallEdge",
+      "signal": false
+    },
+    {
+      "middleLabel": "hasTanggungan (peminjam)",
+      "start": 9,
+      "end": 28,
+      "type": "CallEdge",
+      "signal": false
+    },
+    {
+      "middleLabel": "hasTanggungan ()",
+      "start": 28,
+      "end": 23,
+      "type": "CallEdge",
+      "signal": false
+    },
+    {
+      "middleLabel": "[not hasTanggunganResult] prosesPeminjamanTidakMemilikiTanggungan ()",
+      "start": 9,
+      "end": 10,
+      "type": "CallEdge",
+      "signal": false
+    },
+    {
+      "middleLabel": "«create»",
+      "start": 10,
+      "end": 18,
+      "type": "ConstructorEdge",
+      "signal": false
+    },
+    {
+      "middleLabel": "borrow (isbn)",
+      "start": 10,
+      "end": 20,
+      "type": "CallEdge",
+      "signal": false
+    },
+    {
+      "middleLabel": "findCopyBuku (isbn) -> copyBuku",
+      "start": 20,
+      "end": 21,
+      "type": "CallEdge",
+      "signal": false
+    },
+    {
+      "middleLabel": "isBorrowed ()",
+      "start": 20,
+      "end": 16,
+      "type": "CallEdge",
+      "signal": false
+    },
+    {
+      "middleLabel": "showNotifikasiBerhasilPinjam ()",
+      "start": 10,
+      "end": 11,
+      "type": "CallEdge",
+      "signal": false
+    },
+    {
+      "middleLabel": "[hasTanggungan] showNotifikasiGagalPinjam ()",
+      "start": 9,
+      "end": 12,
+      "type": "CallEdge",
+      "signal": false
+    },
+    {
+      "middleLabel": "[not isValid] showNotifikasiDataTidakValid ()",
+      "start": 8,
+      "end": 13,
+      "type": "CallEdge",
+      "signal": false
+    },
+    {
+      "middleLabel": "buku",
+      "start": 3,
+      "end": 6,
+      "type": "ReturnEdge"
+    },
+    {
+      "middleLabel": "getDetailBuku ()",
+      "start": 6,
+      "end": 1,
+      "type": "CallEdge",
+      "signal": false
+    },
+    {
+      "middleLabel": "detailBuku",
+      "start": 1,
+      "end": 6,
+      "type": "ReturnEdge"
+    },
+    {
+      "middleLabel": "isValid",
+      "start": 27,
+      "end": 8,
+      "type": "ReturnEdge"
+    },
+    {
+      "middleLabel": "hasTanggunganResult",
+      "start": 23,
+      "end": 28,
+      "type": "ReturnEdge"
+    },
+    {
+      "middleLabel": "hasTanggunganResult",
+      "start": 28,
+      "end": 9,
+      "type": "ReturnEdge"
+    }
+  ],
+  "version": "3.8"
+}
+"""
+        parser = ParseJsonToObjectSeq()
+        parser.set_json(json_data)
+        parser.parse()
+
+        parsed_value = parser.get_class_object()
+        self.assertEqual(parsed_value['Buku'].get_method()[0].get_name(), "getDetailBuku")
+
+        self.assertEqual(parsed_value['ListBuku'].get_method()[0].get_name(), "getBuku")
+        self.assertEqual(parsed_value["ListBuku"].get_method()[0].get_parameters()[0].get_name(), "isbn")
+
+        self.assertEqual(parsed_value['ListPeminjam'].get_method()[0].get_name(), "isValid")
+        self.assertEqual(parsed_value["ListPeminjam"].get_method()[0].get_parameters()[0].get_name(), "dataAnggota")
+        self.assertEqual(parsed_value['ListPeminjam'].get_method()[1].get_name(), "hasTanggungan")
+        self.assertEqual(parsed_value["ListPeminjam"].get_method()[1].get_parameters()[0].get_name(), "peminjam")
+
+        self.assertEqual(parsed_value["Peminjam"].get_method()[0].get_name(), "hasTanggungan")
+
+        self.assertEqual(parsed_value["ListCopy"].get_method()[0].get_name(), "borrow")
+        self.assertEqual(parsed_value["ListCopy"].get_method()[0].get_parameters()[0].get_name(), "isbn")
+
+        self.assertEqual(parsed_value["CopyBuku"].get_method()[0].get_name(), "isBorrowed")
+
+    def test_positive_method_call_object(self):
+        json_data = """ 
+{
+  "diagram": "SequenceDiagram",
+  "nodes": [
+    {
+      "children": [
+        1
+      ],
+      "name": "buku:Buku",
+      "x": 510,
+      "y": 123,
+      "id": 0,
+      "type": "ImplicitParameterNode"
+    },
+    {
+      "x": 0,
+      "y": 0,
+      "openBottom": false,
+      "id": 1,
+      "type": "CallNode"
+    },
+    {
+      "children": [
+        3
+      ],
+      "name": "listBuku:ListBuku",
+      "x": 370,
+      "y": 91,
+      "id": 2,
+      "type": "ImplicitParameterNode"
+    },
+    {
+      "x": 0,
+      "y": 0,
+      "openBottom": false,
+      "id": 3,
+      "type": "CallNode"
+    },
+    {
+      "children": [
+        5,
+        6,
+        7,
+        8,
+        9,
+        10,
+        11,
+        12,
+        13,
+        14
+      ],
+      "name": ":views",
+      "x": 250,
+      "y": 52,
+      "id": 4,
+      "type": "ImplicitParameterNode"
+    },
+    {
+      "x": 0,
+      "y": 0,
+      "openBottom": false,
+      "id": 5,
+      "type": "CallNode"
+    },
+    {
+      "x": 0,
+      "y": 0,
+      "openBottom": false,
+      "id": 6,
+      "type": "CallNode"
+    },
+    {
+      "x": 0,
+      "y": 0,
+      "openBottom": false,
+      "id": 7,
+      "type": "CallNode"
+    },
+    {
+      "x": 890,
+      "y": 10,
+      "openBottom": false,
+      "id": 8,
+      "type": "CallNode"
+    },
+    {
+      "x": 335,
+      "y": 56,
+      "openBottom": false,
+      "id": 9,
+      "type": "CallNode"
+    },
+    {
+      "x": 29,
+      "y": 29,
+      "openBottom": false,
+      "id": 10,
+      "type": "CallNode"
+    },
+    {
+      "x": 0,
+      "y": 0,
+      "openBottom": false,
+      "id": 11,
+      "type": "CallNode"
+    },
+    {
+      "x": 0,
+      "y": 0,
+      "openBottom": false,
+      "id": 12,
+      "type": "CallNode"
+    },
+    {
+      "x": 0,
+      "y": 0,
+      "openBottom": false,
+      "id": 13,
+      "type": "CallNode"
+    },
+    {
+      "x": 0,
+      "y": 0,
+      "openBottom": false,
+      "id": 14,
+      "type": "CallNode"
+    },
+    {
+      "children": [
+        16
+      ],
+      "name": "copyBuku:CopyBuku",
+      "x": 1420,
+      "y": -8,
+      "id": 15,
+      "type": "ImplicitParameterNode"
+    },
+    {
+      "x": 0,
+      "y": 0,
+      "openBottom": false,
+      "id": 16,
+      "type": "CallNode"
+    },
+    {
+      "children": [
+        18
+      ],
+      "name": "peminjaman:Peminjaman",
+      "x": 840,
+      "y": 28,
+      "id": 17,
+      "type": "ImplicitParameterNode"
+    },
+    {
+      "x": 0,
+      "y": 0,
+      "openBottom": false,
+      "id": 18,
+      "type": "CallNode"
+    },
+    {
+      "children": [
+        20,
+        21
+      ],
+      "name": "listCopy:ListCopy",
+      "x": 1090,
+      "y": 126,
+      "id": 19,
+      "type": "ImplicitParameterNode"
+    },
+    {
+      "x": 0,
+      "y": 0,
+      "openBottom": false,
+      "id": 20,
+      "type": "CallNode"
+    },
+    {
+      "x": 0,
+      "y": 0,
+      "openBottom": false,
+      "id": 21,
+      "type": "CallNode"
+    },
+    {
+      "children": [
+        23
+      ],
+      "name": "peminjam:Peminjam",
+      "x": 800,
+      "y": 73,
+      "id": 22,
+      "type": "ImplicitParameterNode"
+    },
+    {
+      "x": 0,
+      "y": 0,
+      "openBottom": false,
+      "id": 23,
+      "type": "CallNode"
+    },
+    {
+      "children": [
+        25
+      ],
+      "name": ":UI",
+      "x": 10,
+      "y": 65,
+      "id": 24,
+      "type": "ImplicitParameterNode"
+    },
+    {
+      "x": 26,
+      "y": 18,
+      "openBottom": false,
+      "id": 25,
+      "type": "CallNode"
+    },
+    {
+      "children": [
+        27,
+        28
+      ],
+      "name": "listPeminjam:ListPeminjam",
+      "x": 620,
+      "y": 123,
+      "id": 26,
+      "type": "ImplicitParameterNode"
+    },
+    {
+      "x": 0,
+      "y": 0,
+      "openBottom": false,
+      "id": 27,
+      "type": "CallNode"
+    },
+    {
+      "x": 0,
+      "y": 0,
+      "openBottom": false,
+      "id": 28,
+      "type": "CallNode"
+    }
+  ],
+  "edges": [
+    {
+      "middleLabel": "[POST] login (username, password)",
+      "start": 25,
+      "end": 14,
+      "type": "CallEdge",
+      "signal": false
+    },
+    {
+      "middleLabel": "HalamanPemrosesanPeminjaman ()",
+      "start": 25,
+      "end": 5,
+      "type": "CallEdge",
+      "signal": false
+    },
+    {
+      "middleLabel": "LihatDetailBuku (isbn)",
+      "start": 25,
+      "end": 6,
+      "type": "CallEdge",
+      "signal": false
+    },
+    {
+      "middleLabel": "getBuku (isbn)",
+      "start": 6,
+      "end": 3,
+      "type": "CallEdge",
+      "signal": false
+    },
+    {
+      "middleLabel": "FormProsesPeminjaman (isbn)",
+      "start": 25,
+      "end": 7,
+      "type": "CallEdge",
+      "signal": false
+    },
+    {
+      "middleLabel": "[POST] SubmitProsesPeminjaman (isbn)",
+      "start": 25,
+      "end": 8,
+      "type": "CallEdge",
+      "signal": false
+    },
+    {
+      "middleLabel": "isValid (dataAnggota)",
+      "start": 8,
+      "end": 27,
+      "type": "CallEdge",
+      "signal": false
+    },
+    {
+      "middleLabel": "[isValid] prosesPeminjamanValidKeanggotaan ()",
+      "start": 8,
+      "end": 9,
+      "type": "CallEdge",
+      "signal": false
+    },
+    {
+      "middleLabel": "hasTanggungan (peminjam)",
+      "start": 9,
+      "end": 28,
+      "type": "CallEdge",
+      "signal": false
+    },
+    {
+      "middleLabel": "hasTanggungan ()",
+      "start": 28,
+      "end": 23,
+      "type": "CallEdge",
+      "signal": false
+    },
+    {
+      "middleLabel": "[not hasTanggunganResult] prosesPeminjamanTidakMemilikiTanggungan ()",
+      "start": 9,
+      "end": 10,
+      "type": "CallEdge",
+      "signal": false
+    },
+    {
+      "middleLabel": "«create»",
+      "start": 10,
+      "end": 18,
+      "type": "ConstructorEdge",
+      "signal": false
+    },
+    {
+      "middleLabel": "borrow (isbn)",
+      "start": 10,
+      "end": 20,
+      "type": "CallEdge",
+      "signal": false
+    },
+    {
+      "middleLabel": "findCopyBuku (isbn) -> copyBuku",
+      "start": 20,
+      "end": 21,
+      "type": "CallEdge",
+      "signal": false
+    },
+    {
+      "middleLabel": "isBorrowed ()",
+      "start": 20,
+      "end": 16,
+      "type": "CallEdge",
+      "signal": false
+    },
+    {
+      "middleLabel": "showNotifikasiBerhasilPinjam ()",
+      "start": 10,
+      "end": 11,
+      "type": "CallEdge",
+      "signal": false
+    },
+    {
+      "middleLabel": "[hasTanggungan] showNotifikasiGagalPinjam ()",
+      "start": 9,
+      "end": 12,
+      "type": "CallEdge",
+      "signal": false
+    },
+    {
+      "middleLabel": "[not isValid] showNotifikasiDataTidakValid ()",
+      "start": 8,
+      "end": 13,
+      "type": "CallEdge",
+      "signal": false
+    },
+    {
+      "middleLabel": "buku",
+      "start": 3,
+      "end": 6,
+      "type": "ReturnEdge"
+    },
+    {
+      "middleLabel": "getDetailBuku ()",
+      "start": 6,
+      "end": 1,
+      "type": "CallEdge",
+      "signal": false
+    },
+    {
+      "middleLabel": "detailBuku",
+      "start": 1,
+      "end": 6,
+      "type": "ReturnEdge"
+    },
+    {
+      "middleLabel": "isValid",
+      "start": 27,
+      "end": 8,
+      "type": "ReturnEdge"
+    },
+    {
+      "middleLabel": "hasTanggunganResult",
+      "start": 23,
+      "end": 28,
+      "type": "ReturnEdge"
+    },
+    {
+      "middleLabel": "hasTanggunganResult",
+      "start": 28,
+      "end": 9,
+      "type": "ReturnEdge"
+    }
+  ],
+  "version": "3.8"
+}
+"""
+        parser = ParseJsonToObjectSeq()
+        parser.set_json(json_data)
+        parser.parse()
+
+        parsed_value_class_object = parser.get_class_object()
+        parsed_value_controller = parser.get_controller_method()
+
+        self.assertEqual(len(parsed_value_controller[0].get_call()), 0)
+
+        self.assertEqual(len(parsed_value_controller[1].get_call()), 0)
+
+        self.assertEqual(len(parsed_value_controller[2].get_call()), 2)
+        self.assertEqual(parsed_value_controller[2].get_call()[0].get_method().get_name(), "getBuku")
+        self.assertEqual(parsed_value_controller[2].get_call()[1].get_method().get_name(), "getDetailBuku")
+
+        self.assertEqual(len(parsed_value_controller[3].get_call()), 0)
+
+        self.assertEqual(len(parsed_value_controller[4].get_call()), 3)
+        self.assertEqual(parsed_value_controller[4].get_call()[0].get_method().get_name(), "isValid")
+        self.assertEqual(parsed_value_controller[4].get_call()[1].get_method().get_name(), "prosesPeminjamanValidKeanggotaan")
+        self.assertEqual(parsed_value_controller[4].get_call()[1].get_method().get_call()[0].get_method().get_name(), "hasTanggungan")
+        self.assertEqual(parsed_value_controller[4].get_call()[1].get_method().get_call()[0].get_method().get_call()[0].get_method().get_name(), "hasTanggungan")
+        self.assertEqual(parsed_value_controller[4].get_call()[1].get_method().get_call()[1].get_method().get_name(), "prosesPeminjamanTidakMemilikiTanggungan")
+        self.assertEqual(parsed_value_controller[4].get_call()[1].get_method().get_call()[1].get_method().get_call()[0].get_method().get_name(), "borrow")
+        self.assertEqual(parsed_value_controller[4].get_call()[1].get_method().get_call()[1].get_method().get_call()[0].get_method().get_call()[0].get_method().get_name(), "findCopyBuku")
+        self.assertEqual(parsed_value_controller[4].get_call()[1].get_method().get_call()[1].get_method().get_call()[0].get_method().get_call()[1].get_method().get_name(), "isBorrowed")
+        self.assertEqual(parsed_value_controller[4].get_call()[1].get_method().get_call()[1].get_method().get_call()[1].get_method().get_name(), "showNotifikasiBerhasilPinjam")
+        self.assertEqual(parsed_value_controller[4].get_call()[1].get_method().get_call()[2].get_method().get_name(), "showNotifikasiGagalPinjam")
+        self.assertEqual(parsed_value_controller[4].get_call()[2].get_method().get_name(), "showNotifikasiDataTidakValid")
+
+        self.assertEqual(len(parsed_value_class_object['ListBuku'].get_method()[0].get_call()), 0)
+        self.assertEqual(len(parsed_value_class_object['Buku'].get_method()[0].get_call()), 0)
+
+        self.assertEqual(len(parsed_value_class_object['ListPeminjam'].get_method()[0].get_call()), 0)
