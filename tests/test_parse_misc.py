f799802bfa119e4eb8635803fa858f9854a69f91
Clarence Grady <clarence.grady@ui.ac.id>
2025-03-09 13:29:20 +0700

    [RED] add more test for parse_return_edge method

--- tests/test_parse_misc.py
+++ tests/test_parse_misc.py
@@ -7,16 +7,36 @@ class TestParseMisc(unittest.TestCase):
     def setUp(self):
         self.parser = ParseJsonToObjectSeq()
 
-    def test_set_return_edge_positive(self):
+    def test_parse_return_edge_positive(self):
         with open("tests/testdata/parse_misc_positive.json") as file:
             json_data = file.read().replace("\n", "")
             self.parser.set_json(json_data)
             self.parser.parse()
-            self.assertEqual(self.parser.get_return_edge(), "Success")
+            print(self.parser.get_method_call())
+            self.assertEqual(self.parser.parse_return_edge(), "Success")
 
-    def test_set_return_edge_negative(self):
-        with open("tests/testdata/parse_misc_negative.json") as file:
+    def test_parse_return_edge_label_invalid(self):
+        with open("tests/testdata/parse_misc_negative1.json") as file:
             json_data = file.read().replace("\n", "")
             self.parser.set_json(json_data)
             self.parser.parse()
-            self.assertEqual(self.parser.get_return_edge(), "Error")
+
+            with self.assertRaises(ValueError) as context:
+                self.parser.parse_return_edge()
+                self.assertEqual(
+                    context.exception,
+                    "Return edge label must be a valid variable name! Given: @return",
+                )
+
+    def test_parse_return_edge_no_call_edge(self):
+        with open("tests/testdata/parse_misc_negative2.json") as file:
+            json_data = file.read().replace("\n", "")
+            self.parser.set_json(json_data)
+            self.parser.parse()
+
+            with self.assertRaises(ValueError) as context:
+                self.parser.parse_return_edge()
+                self.assertEqual(
+                    context.exception,
+                    f"Return edge must have a corresponding call edge! {6} -> {3}",
+                )
