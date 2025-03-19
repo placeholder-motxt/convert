e5640f7c5afa6fd11a4ede9e61b70904ae7f15e8
Clarence Grady <clarence.grady@ui.ac.id>
2025-03-19 10:52:06 +0700

    [RED] Add tests for generating django project

--- tests/test_render_project_django.py
+++ tests/test_render_project_django.py
@@ -1,6 +1,7 @@
 import os
 import shutil
 import unittest
+import zipfile
 
 from app.main import create_django_project
 from app.utils import render_project_django_template
@@ -9,6 +10,10 @@ from app.utils import render_project_django_template
 class TestGenerateDjangoProjectTemplate(unittest.TestCase):
     def setUp(self):
         self.project_name = "test_project"
+        if os.path.exists(f"{self.project_name}.zip"):
+            os.remove(f"{self.project_name}.zip")
+        if os.path.exists(f"project_{self.project_name}"):
+            shutil.rmtree(f"project_{self.project_name}")
 
     # generate django project
     def test_generate_django_project_positive(self):
@@ -31,25 +36,34 @@ class TestGenerateDjangoProjectTemplate(unittest.TestCase):
             if os.path.exists(folder_path):
                 shutil.rmtree(folder_path)
 
-    def test_generate_django_project_positive_with_whitespace(self):
-        result = create_django_project("test project")
-        zipfile_path = "test_project.zip"
-        folder_path = "project_test_project"
+    def test_generate_django_project_negative_with_whitespace(self):
+        with self.assertRaises(ValueError) as context:
+            create_django_project("test project")
+        self.assertEqual(
+            str(context.exception),
+            "Project name must not contain whitespace or number!",
+        )
+        if os.path.exists("project_test_project"):
+            shutil.rmtree("project_test_project")
+        if os.path.exists("test_project.zip"):
+            os.remove("test_project.zip")
+
+    def test_generate_django_project_negative_with_existing_zipfile(self):
+        zipfile_path = f"{self.project_name}.zip"
+        with zipfile.ZipFile(zipfile_path, "w") as zipf:
+            zipf.writestr(self.project_name, data="")  # create empty file
         try:
-            self.assertTrue(os.path.exists(folder_path))
-            for file in os.listdir(folder_path):
-                self.assertIn(
-                    file, ["asgi.py", "manage.py", "settings.py", "urls.py", "wsgi.py"]
-                )
-            self.assertTrue(os.path.exists(zipfile_path))
+            with self.assertRaises(ValueError) as context:
+                create_django_project(self.project_name)
             self.assertEqual(
-                result, ["asgi.py", "manage.py", "settings.py", "urls.py", "wsgi.py"]
+                str(context.exception),
+                f"File {self.project_name}.zip already exists",
             )
         finally:
             if os.path.exists(zipfile_path):
                 os.remove(zipfile_path)
-            if os.path.exists(folder_path):
-                shutil.rmtree(folder_path)
+        if os.path.exists("project_test_project"):
+            shutil.rmtree("project_test_project")
 
     # render django project from template
     def test_render_project_django_template_positive(self):
@@ -70,19 +84,29 @@ class TestGenerateDjangoProjectTemplate(unittest.TestCase):
             if os.path.exists(folder_path):
                 shutil.rmtree(folder_path)
 
-    def test_render_project_django_template_positive_with_whitespace(self):
-        folder_path = "project_test_project"
-        try:
-            result = render_project_django_template(
+    def test_render_project_django_template_negative_with_whitespace(self):
+        with self.assertRaises(ValueError) as context:
+            render_project_django_template(
                 "app/templates/django_project", {"project_name": "test project"}
             )
-            self.assertTrue(os.path.exists(folder_path))
-            for file in os.listdir(folder_path):
-                self.assertIn(
-                    file, ["asgi.py", "manage.py", "settings.py", "urls.py", "wsgi.py"]
+        self.assertEqual(
+            str(context.exception),
+            "Project name must not contain whitespace or number!",
+        )
+        if os.path.exists("project_test_project"):
+            shutil.rmtree("project_test_project")
+
+    def test_render_project_django_template_negative_with_existing_folder(self):
+        folder_path = "project_test_project"
+        os.makedirs(folder_path, exist_ok=True)
+        try:
+            with self.assertRaises(FileExistsError) as context:
+                render_project_django_template(
+                    "app/templates/django_project", {"project_name": self.project_name}
                 )
             self.assertEqual(
-                result, ["asgi.py", "manage.py", "settings.py", "urls.py", "wsgi.py"]
+                str(context.exception),
+                "Cannot create a file when that file already exists",
             )
         finally:
             if os.path.exists(folder_path):
