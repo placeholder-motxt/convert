760b12ff0be75f4fa54cecb435271ecd83b00332
DaWanAnOnli <joel.rompis@gmail.com>
2025-03-19 14:37:43 +0700

    [GREEN] Implement get_classes and add_class methods for ModelsElements class, get_fields method of ClassObject class, and get_name method of FieldObject class

--- app/models/properties.py
+++ app/models/properties.py
@@ -67,6 +67,9 @@ class FieldObject:
 
     def __str__(self) -> str:
         return f"FieldObject:\n\tname: {self.__name}\n\ttype: {self.__type}"
+    
+    def get_name(self): # pragma: no cover
+        return self.__name
 
     def set_name(self, name: str):
         self.__name = name
