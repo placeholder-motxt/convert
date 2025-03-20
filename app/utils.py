2cd415ed4a0c3ba0d0573965bab383b6d15d2bc2
DaWanAnOnli <joel.rompis@gmail.com>
2025-03-20 17:26:15 +0700

    [GREEN] Implement camel_to_snake function in utils.py

--- app/utils.py
+++ app/utils.py
@@ -1,4 +1,5 @@
 import os
+import re
 from keyword import iskeyword
 from typing import Any
 
@@ -18,3 +19,12 @@ def is_valid_python_identifier(identifier: str) -> bool:
 def render_template(template_name: str, context: dict[str, Any]) -> str:
     template = env.get_template(template_name)
     return template.render(context)
+
+
+def camel_to_snake(camel_case_str: str) -> str:
+    # Adjust regex to handle acronyms properly (uppercase letters in the middle of the string)
+    snake_case_str = re.sub(r"([a-z0-9])([A-Z])", r"\1_\2", camel_case_str)
+    # Special handling for acronyms: Make sure that sequences of uppercase letters are also split
+    # correctly.
+    snake_case_str = re.sub(r"([A-Z]+)([A-Z][a-z])", r"\1_\2", snake_case_str)
+    return snake_case_str.lower()
