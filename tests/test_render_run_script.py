7b47ad6cfab710c174ec4ea5fb916fcab034ecc8
Will S <williamsusanto82@gmail.com>
2025-03-24 09:00:33 +0700

    [RED] Create tests for rendering the requirements.txt

---
+++ tests/test_render_run_script.py
@@ -0,0 +1,23 @@
+import unittest
+
+from app.models.elements import RunBashScriptElements, RunBatScriptElements
+
+
+class TestRenderRunScript(unittest.TestCase):
+    def setUp(self):
+        self.bash_script_element = RunBashScriptElements("run.sh")
+        self.bat_script_element = RunBatScriptElements("run.bat")
+
+    def test_bash_print_django_style(self):
+        with open("tests/test_run_sh.txt", "r", encoding="utf-8") as file:
+            sh = file.read()
+
+        rendered = self.bash_script_element.print_django_style()
+        self.assertEqual(rendered, sh)
+
+    def test_bat_print_django_style(self):
+        with open("tests/test_run_bat.txt", "r", encoding="utf-8") as file:
+            bat = file.read()
+
+        rendered = self.bat_script_element.print_django_style()
+        self.assertEqual(rendered, bat)
