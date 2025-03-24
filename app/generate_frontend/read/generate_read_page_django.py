a11b352366b42cbd527323d75f891edbe5ef7782
DaWanAnOnli <joel.rompis@gmail.com>
2025-03-24 11:53:12 +0700

    [GREEN] implement argument type validation for generate_html_read_pages_django and generate_html_read_page_django functions

--- app/generate_frontend/read/generate_read_page_django.py
+++ app/generate_frontend/read/generate_read_page_django.py
@@ -4,6 +4,8 @@ from app.utils import camel_to_snake, render_template
 
 
 def generate_html_read_pages_django(models_elements: ModelsElements) -> list[str]:
+    if not isinstance(models_elements, ModelsElements):
+        raise (TypeError("argument must be of type ModelsElements!"))
     result = []
     for class_object in models_elements.get_classes():
         result.append(generate_html_read_page_django(class_object))
@@ -11,6 +13,8 @@ def generate_html_read_pages_django(models_elements: ModelsElements) -> list[str
 
 
 def generate_html_read_page_django(class_object: ClassObject) -> str:
+    if not isinstance(class_object, ClassObject):
+        raise (TypeError("argument must be of type ClassObject!"))
     context = {
         "class_name": class_object.get_name(),
         "class_snake": camel_to_snake(class_object.get_name()),
