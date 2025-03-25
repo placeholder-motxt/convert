337830e794dc291a6ff6fc01f8f74a9c0a2d7658
DaWanAnOnli <joel.rompis@gmail.com>
2025-03-25 08:26:43 +0700

    [REFACTOR] Add comment on result list for generate_html_read_pages_django function, and use string comprehension for fields in generate_html_read_page_django.

--- app/generate_frontend/read/generate_read_page_django.py
+++ app/generate_frontend/read/generate_read_page_django.py
@@ -5,7 +5,10 @@ from app.utils import camel_to_snake, render_template
 
 def generate_html_read_pages_django(models_elements: ModelsElements) -> list[str]:
     if not isinstance(models_elements, ModelsElements):
-        raise (TypeError("argument must be of type ModelsElements!"))
+        raise TypeError(
+            f"Expected type ModelsElements, got {type(models_elements)} instead"
+        )
+    # Consider changing the code below to avoid memory explosion
     result = []
     for class_object in models_elements.get_classes():
         result.append(generate_html_read_page_django(class_object))
@@ -14,13 +17,11 @@ def generate_html_read_pages_django(models_elements: ModelsElements) -> list[str
 
 def generate_html_read_page_django(class_object: ClassObject) -> str:
     if not isinstance(class_object, ClassObject):
-        raise (TypeError("argument must be of type ClassObject!"))
+        raise TypeError(f"Expected type CLassObject, got {type(class_object)} instead")
     context = {
         "class_name": class_object.get_name(),
         "class_snake": camel_to_snake(class_object.get_name()),
     }
-    fields = []
-    for field in class_object.get_fields():
-        fields.append({"name": field.get_name()})
+    fields = [{"name": f.get_name()} for f in class_object.get_fields()]
     context["fields"] = fields
     return render_template("read_page_django.html.j2", context)
