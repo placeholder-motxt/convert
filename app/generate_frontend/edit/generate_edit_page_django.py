277e8a5d066feb9f1926a0ff2719a87813a1d970
MightyZanark <40352213+MightyZanark@users.noreply.github.com>
2025-03-26 19:45:11 +0900

    [REFACTOR] remove redundant checks to just one

--- app/generate_frontend/edit/generate_edit_page_django.py
+++ app/generate_frontend/edit/generate_edit_page_django.py
@@ -4,16 +4,32 @@ from app.utils import render_template
 
 
 def generate_html_edit_pages_django(models_elements: ModelsElements) -> list[str]:
+    """
+    Generates edit page HTML of all ClassObject inside of ModelsElement
+    If a ClassObject somehow doesn't have a name nor fields, the edit
+    page will not be generated.
+
+    Returns a list of string where each string is HTML that corresponds
+    to each non empty ClassObject in ModelsElement.
+    """
+
     result = []
     for class_object in models_elements.get_classes():
-        if class_object.get_name() == "" or len(class_object.get_fields()) == 0:
-            continue
+        html = generate_html_edit_page_django(class_object)
+        if html != "":
+            result.append(html)
 
-        result.append(generate_html_edit_page_django(class_object))
     return result
 
 
 def generate_html_edit_page_django(class_object: ClassObject) -> str:
+    """
+    Generates edit page HTML of a ClassObject if it has a name and fields.
+
+    Returns a string representation of the HTML page or an empty string
+    if the ClassObject doesn't have a name nor fields.
+    """
+
     if class_object.get_name() == "" or len(class_object.get_fields()) == 0:
         return ""
 
