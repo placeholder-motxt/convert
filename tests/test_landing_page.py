b678b4a01061a1996c0e06886c63fccf8c91ed3c
KenKomKom <kenichikomala@gmail.com>
2025-04-07 19:15:39 +0700

    [RED] Change test to handle private in landingpage

--- tests/test_landing_page.py
+++ tests/test_landing_page.py
@@ -18,7 +18,7 @@ class TestLandingPageFunctions(unittest.TestCase):
 
     def test_generate_landing_page_views(self):
         # Call the function
-        result = generate_landing_page_views()
+        result = generate_landing_page_views([])
 
         # Assert render_template was called with the correct template and context
         self.assertIn("model", result)
