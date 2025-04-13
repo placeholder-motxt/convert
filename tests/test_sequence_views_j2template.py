97a7943ce53a50dcd0156bf2d05434a2f821bb0b
DaWanAnOnli <joel.rompis@gmail.com>
2025-04-13 19:42:44 +0700

    [RED] Add negative tests for test_sequence_views_j2template.py

--- tests/test_sequence_views_j2template.py
+++ tests/test_sequence_views_j2template.py
@@ -1,6 +1,8 @@
 import os
 import unittest
 
+import pytest
+
 from app.models.elements import ViewsElements
 from app.models.methods import (
     ArgumentObject,
@@ -15,7 +17,7 @@ TEST_DIR = os.path.join(CUR_DIR, "testdata")
 
 
 class TestSequenceViewsJinja2Template(unittest.TestCase):
-    def setUp(self):
+    def test_sequence_with_all_features(self):
         str_type = TypeObject()
         str_type.set_name("string")
 
@@ -51,48 +53,101 @@ class TestSequenceViewsJinja2Template(unittest.TestCase):
         self.views_elements.add_class_method(self.class_method_1)
         self.views_elements.add_controller_method(self.controller_method_1)
 
-    def test_class_with_all_features(self):
         with open(os.path.join(TEST_DIR, "sequence_j2.txt")) as f:
             expected = f.read().strip()
 
         result = self.views_elements.print_django_style_template().strip()
         self.assertEqual(result, expected)
 
+    def test_sequence_no_method(self):
+        expected = ""
+
+        views_elements = ViewsElements("views.py")
+        result = views_elements.print_django_style_template().strip()
+        self.assertEqual(result, expected)
+
+    def test_sequence_invalid_class_method_name(self):
+        class_method_1 = ClassMethodObject()
+        class_method_1.set_name("12334")
+
+        views_elements = ViewsElements("views.py")
+        views_elements.add_class_method(class_method_1)
+        with pytest.raises(ValueError):
+            views_elements.print_django_style_template()
+
+    def test_sequence_invalid_param_name(self):
+        class_method_1 = ClassMethodObject()
+        class_method_1.set_name("class_method_1")
+
+        param_1 = ParameterObject()
+        param_1.set_name("1234")
+
+        class_method_1.add_parameter(param_1)
+
+        views_elements = ViewsElements("views.py")
+        views_elements.add_class_method(class_method_1)
+        with pytest.raises(ValueError):
+            views_elements.print_django_style_template()
+
+    def test_sequence_invalid_controller_method_name(self):
+        controller_method_1 = ControllerMethodObject()
+
+        views_elements = ViewsElements("views.py")
+        views_elements.add_controller_method(controller_method_1)
+        with pytest.raises(ValueError):
+            views_elements.print_django_style_template()
+
+    def test_sequence_invalid_return_type(self):
+        class_method_1 = ClassMethodObject()
+        class_method_1.set_name("class_method_1")
+
+        type_object = TypeObject()
+        type_object.set_name("1ello")
+
+        class_method_1.set_return_type(type_object)
+
+        views_elements = ViewsElements("views.py")
+        views_elements.add_class_method(class_method_1)
+        with pytest.raises(ValueError):
+            views_elements.print_django_style_template()
+
+    def test_sequence_list_return_type(self):
+        class_method_1 = ClassMethodObject()
+        class_method_1.set_name("class_method_1")
+
+        type_object = TypeObject()
+        type_object.set_name("list[string]")
+
+        class_method_1.set_return_type(type_object)
+
+        views_elements = ViewsElements("views.py")
+        views_elements.add_class_method(class_method_1)
+
+        with open(os.path.join(TEST_DIR, "sequence_j2_list_return_type.txt")) as f:
+            expected = f.read().strip()
+
+        result = views_elements.print_django_style_template().strip()
+        self.assertEqual(result, expected)
+
+    def test_sequence_invalid_param_type(self):
+        class_method_1 = ClassMethodObject()
+        class_method_1.set_name("class_method_1")
+
+        param_1 = ParameterObject()
+        param_1.set_name("hello")
+
+        type_object = TypeObject()
+        type_object.set_name("1ello")
+
+        param_1.set_type(type_object)
+
+        class_method_1.add_parameter(param_1)
+
+        views_elements = ViewsElements("views.py")
+        views_elements.add_class_method(class_method_1)
+        with pytest.raises(ValueError):
+            views_elements.print_django_style_template()
+
 
 if __name__ == "__main__":
-    str_type = TypeObject()
-    str_type.set_name("string")
-
-    class_method_1 = ClassMethodObject()
-    class_method_1.set_name("class_method_1")
-    class_method_1.set_return_type(str_type)
-
-    parameter_1 = ParameterObject()
-    parameter_1.set_name("parameter_1")
-    parameter_1.set_type(str_type)
-
-    class_method_1.add_parameter(parameter_1)
-
-    class_method_call_1 = ClassMethodCallObject()
-    class_method_call_1.set_method(class_method_1)
-    # class_method_call_1.set_condition("condition == True")
-    class_method_call_1.set_ret_var("ret_var_1")
-    class_method_call_1.set_instance_name("instance_1")
-
-    argument_1 = ArgumentObject()
-    argument_1.set_method_object(class_method_call_1)
-    argument_1.set_name("argument_1")
-    argument_1.set_type(str_type)
-
-    class_method_call_1.add_argument(argument_1)
-
-    class_method_1.add_class_method_call(class_method_call_1)
-
-    controller_method_1 = ControllerMethodObject()
-    controller_method_1.set_name("controller_method_1")
-
-    views_elements = ViewsElements("views.py")
-    views_elements.add_class_method(class_method_1)
-    views_elements.add_controller_method(controller_method_1)
-
-    print(views_elements.print_django_style_template())
+    pass
