7ffcf590ba27669477fbaef93e5409a79ef477dc
KenKomKom <kenichikomala@gmail.com>
2025-04-17 11:46:40 +0700

    [RED] Add test for SQLiteDialect

---
+++ tests/test_sqlitedialect.py
@@ -0,0 +1,16 @@
+import os
+import unittest
+
+from app.generate_sqlite_config.generate_sqlite_config import generate_sqlitedialect
+
+CUR_DIR = os.path.dirname(os.path.realpath(__file__))
+TEST_DIR = os.path.join(CUR_DIR, "testdata")
+
+
+class TestRenderRunScript(unittest.TestCase):
+    def test_generate_sqlite_config(self):
+        with open(os.path.join(TEST_DIR, "test_SQLiteDialect.txt")) as file:
+            expected = file.read().strip()
+
+        rendered = generate_sqlitedialect("projek")
+        self.assertEqual(rendered.strip(), expected)
