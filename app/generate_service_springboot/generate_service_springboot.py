3f47f39aabdfdc5cd46071e9ba275c2c16c8996a
Will S <williamsusanto82@gmail.com>
2025-04-18 12:03:11 +0700

    [CHORE] Added docstring for documentation

--- app/generate_service_springboot/generate_service_springboot.py
+++ app/generate_service_springboot/generate_service_springboot.py
@@ -3,6 +3,15 @@ from app.utils import render_template
 
 
 def generate_service_java(project_name: str, model: ClassObject) -> str:
+    """
+    This method is used to generate Service.java for single ClassObject
+
+    Example: If the ClassObject name is Cart, then it will generate
+    CartService.java
+
+    In the main.py, you need to loop for each ClassObject and call the
+    generate_service_java to create separate service based on the Class
+    """
     class_name_capital, class_name = format_class_name(model.get_name())
     context = {
         "project_name": project_name,
