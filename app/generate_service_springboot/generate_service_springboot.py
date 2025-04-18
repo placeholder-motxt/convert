09c5426bfe7817dce7cdd3e7dfd69f24b63e686a
Will S <williamsusanto82@gmail.com>
2025-04-18 09:13:10 +0700

    [REFACTOR & REFINE] Refactor to format the class name to be a new method so there' no function call in the context

--- app/generate_service_springboot/generate_service_springboot.py
+++ app/generate_service_springboot/generate_service_springboot.py
@@ -3,10 +3,13 @@ from app.utils import render_template
 
 
 def generate_service_java(project_name: str, model: ClassObject) -> str:
-    model_name = model.get_name()
+    class_name_capital, class_name = format_class_name(model.get_name())
     context = {
         "project_name": project_name,
-        "class_name_capital": model_name.capitalize(),
-        "class_name": model_name[0].lower() + model_name[1:],
+        "class_name_capital": class_name_capital,
+        "class_name": class_name,
     }
     return render_template("springboot/service.java.j2", context)
+
+def format_class_name(name: str):
+    return name.capitalize(), name[0].lower() + name[1:]
