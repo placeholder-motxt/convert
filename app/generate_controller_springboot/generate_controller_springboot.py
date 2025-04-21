5678727f08a96d5551b720ceb380ffbfc655d2de
DaWanAnOnli <joel.rompis@gmail.com>
2025-04-21 12:07:35 +0700

    [GREEN] Implement generate_springboot_controller_files function

--- app/generate_controller_springboot/generate_controller_springboot.py
+++ app/generate_controller_springboot/generate_controller_springboot.py
@@ -1,9 +1,22 @@
 import logging
 
 from app.models.diagram import ClassObject
+from app.models.elements import ModelsElements
 from app.utils import render_template, to_camel_case, to_pascal_case
 
 logger = logging.getLogger("uvicorn.error")
+
+
+def generate_springboot_controller_files(
+    project_name: str, models_elements: ModelsElements
+) -> list[str]:
+    result = []
+    for class_object in models_elements.get_classes():
+        if class_object.get_is_public():
+            result.append(
+                generate_springboot_controller_file(project_name, class_object)
+            )
+    return result
 
 
 def generate_springboot_controller_file(
