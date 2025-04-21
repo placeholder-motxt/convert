d0a0127bdcbc389428078f5390098c0f4fa05687
DaWanAnOnli <joel.rompis@gmail.com>
2025-04-21 11:36:32 +0700

    [GREEN] Implement generate_controller_springboot.py function with corresponding Jinja template

---
+++ app/generate_controller_springboot/generate_controller_springboot.py
@@ -0,0 +1,25 @@
+import logging
+
+from app.models.diagram import ClassObject
+from app.utils import render_template, to_camel_case, to_pascal_case
+
+logger = logging.getLogger("uvicorn.error")
+
+
+def generate_springboot_controller_file(
+    project_name: str, class_object: ClassObject
+) -> str:
+    class_name = class_object.get_name()
+    if not class_name:
+        logger.error("Class name cannot be empty!")
+        return ""
+    context = {
+        "class_pascal": to_pascal_case(class_name),
+        "class_camel": to_camel_case(class_name),
+        "project_name": project_name,
+    }
+    try:
+        return render_template("springboot_controller.java.j2", context)
+    except Exception as e:
+        logger.error(f"Error rendering template: {e}")
+        return ""
