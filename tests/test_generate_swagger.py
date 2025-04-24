57b5ca5d6542b2d012dbf0ac19cd7785b68ca880
KenKomKom <kenichikomala@gmail.com>
2025-04-24 21:41:28 +0700

    [RED] Change test for SwaggerConfig

--- tests/test_generate_swagger.py
+++ tests/test_generate_swagger.py
@@ -16,7 +16,7 @@ class TestGenerateSwagger(unittest.TestCase):
     def test_generate_swagger_config(self):
         with open(os.path.join(TEST_DIR, "test_swagger_config.txt")) as f:
             expected = f.read().strip()
-            res = generate_swagger_config("project")
+            res = generate_swagger_config("com.example", "project")
             self.assertEqual(res, expected)
 
     def test_get_swagger_controller_import(self):
