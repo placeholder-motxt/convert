0db978d49a5ba1f1db0b4ac5a7051ab650f9599d
Clarence Grady <clarence.grady@ui.ac.id>
2025-04-29 22:10:21 +0700

    [RED] change test to add cascade relationship

--- tests/test_diagram.py
+++ tests/test_diagram.py
@@ -383,7 +383,7 @@ class TestToSpringbootModelsTemplate(unittest.TestCase):
         relationship.set_source_class_own_amount("1+")
         expected_output = {
             "name": "private TargetClass targetClass;",
-            "type": "@OneToOne",
+            "type": "@OneToOne(cascade = CascadeType.ALL)",
             "join": '@JoinColumn(name = "source_class_id")',
         }
         self.assertEqual(relationship.to_springboot_models_template(), expected_output)
@@ -395,7 +395,7 @@ class TestToSpringbootModelsTemplate(unittest.TestCase):
         relationship.set_source_class_own_amount("1")
         expected_output = {
             "name": "private TargetClass targetClass;",
-            "type": '@ManyToOne(mappedBy="source_class_id")\n'
+            "type": '@ManyToOne(mappedBy="source_class_id")\n\t'
             '@JsonIgnoreProperties("source_classs")',
             "join": None,
         }
@@ -408,7 +408,7 @@ class TestToSpringbootModelsTemplate(unittest.TestCase):
         relationship.set_source_class_own_amount("*")
         expected_output = {
             "name": "private List<TargetClass> targetClasss;",
-            "type": "@OneToMany\n@JsonIgnore",
+            "type": "@OneToMany(cascade = CascadeType.ALL)\n\t@JsonIgnore",
             "join": '@JoinColumn(name = "source_class_id")',
         }
         self.assertEqual(relationship.to_springboot_models_template(), expected_output)
@@ -420,11 +420,11 @@ class TestToSpringbootModelsTemplate(unittest.TestCase):
         relationship.set_source_class_own_amount("1")
         expected_output = {
             "name": "private List<TargetClass> listOfTargetClasss;",
-            "type": "@ManyToMany\n@JsonIgnore",
+            "type": "@ManyToMany(cascade = CascadeType.ALL)\n\t@JsonIgnore",
             "join": "@JoinTable("
-            '\n\tname = "source_class_target_class",'
-            '\n\tjoinColumns = @JoinColumn(name = "source_class_id"),'
-            '\n\tinverseJoinColumns = @JoinColumn(name = "target_class_id")\n)',
+            '\n\t\tname = "source_class_target_class",'
+            '\n\t\tjoinColumns = @JoinColumn(name = "source_class_id"),'
+            '\n\t\tinverseJoinColumns = @JoinColumn(name = "target_class_id")\n\t)',
         }
         self.assertEqual(relationship.to_springboot_models_template(), expected_output)
 
@@ -450,11 +450,11 @@ class TestToSpringbootModelsTemplate(unittest.TestCase):
         relationship.set_target_class(self.target_class)
         expected_output = {
             "name": "private List<> listOfs;",
-            "type": "@ManyToMany\n@JsonIgnore",
+            "type": "@ManyToMany(cascade = CascadeType.ALL)\n\t@JsonIgnore",
             "join": "@JoinTable("
-            '\n\tname = "_",'
-            '\n\tjoinColumns = @JoinColumn(name = "_id"),'
-            '\n\tinverseJoinColumns = @JoinColumn(name = "_id")\n)',
+            '\n\t\tname = "_",'
+            '\n\t\tjoinColumns = @JoinColumn(name = "_id"),'
+            '\n\t\tinverseJoinColumns = @JoinColumn(name = "_id")\n\t)',
         }
         self.assertEqual(relationship.to_springboot_models_template(), expected_output)
 
